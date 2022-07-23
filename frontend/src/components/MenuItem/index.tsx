54385e141f2126d3ce988b421fc4b84f48c72290
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-07-23 16:10:12 +0100

    MenuItem component styling

--- frontend/src/components/MenuItem/index.tsx
+++ frontend/src/components/MenuItem/index.tsx
@@ -1,10 +1,13 @@
 import { IMenuItem } from 'utils/Types/MenuItem';
 import { Link } from 'react-router-dom';
+import './styles.css';
 
 function MenuItem({ To, Text }: IMenuItem) {
   return (
     <Link to={To}>
-      Item de Menu - {Text}
+      <li className="menuItemContainer">
+        {Text}
+      </li>
     </Link>
   );
 };
