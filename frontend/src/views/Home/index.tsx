dfb3a5cab2260ccec3253a90e53567e1d9e004c8
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-07-24 23:03:56 +0100

    About component creation

--- frontend/src/views/Home/index.tsx
+++ frontend/src/views/Home/index.tsx
@@ -2,6 +2,7 @@ import { BreadcrumbHome } from 'utils/Mocks/Breadcrumb';
 import Header from "components/Header";
 import ArticleSection from 'components/ArticleSection';
 import Breadcrumb from "components/Breadcrumb";
+import About from 'components/About';
 import ArticleAside from 'components/ArticleAside';
 import Footer from "components/Footer";
 
@@ -20,16 +21,7 @@ function Home() {
           Path={BreadcrumbHome.Path}
         />
 
-        <h2>A Empresa</h2>
-        <p>
-          A Companhia do Saldo, mais conhecida como Saldão, está há quase 8 anos
-          no mercado. Uma loja de tecidos, especificamente de malhas, oferecendo
-          serviços de qualidade, confiança e credibilidade.
-        </p>
-
-        <h3>AQUI VAI UMA IMAGEM</h3>
-        <h3>Companhia do Saldo</h3>
-        <p>Venha conhecer.</p>
+        <About />
 
         <h2>Localização</h2>
         <p>
