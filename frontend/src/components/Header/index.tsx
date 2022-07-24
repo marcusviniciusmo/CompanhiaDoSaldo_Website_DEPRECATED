674ce53326ab9b27b9f34dc768f9c02a7ea360c4
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-07-24 07:23:55 +0100

    Header component styling

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,16 +1,16 @@
 import { HeaderData } from 'utils/Mocks/Header';
 import Menu from "components/Menu";
+import './styles.css';
 
 function Header() {
   return (
-    <>
-      <h1>HEADER Component</h1>
+    <header id='headerContainer'>
       <h1>{HeaderData.Title}</h1>
 
       <img src={HeaderData.Image.Url} alt={HeaderData.Image.Description} />
 
       <Menu />
-    </>
+    </header>
   );
 };
 
