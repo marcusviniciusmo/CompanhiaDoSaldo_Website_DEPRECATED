1db42c08331c2314c112143fff5a404d56292ea1
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-07-26 10:40:12 +0100

    News component styling

--- frontend/src/components/News/index.tsx
+++ frontend/src/components/News/index.tsx
@@ -1,14 +1,18 @@
+import './styles.css';
+
 function News() {
   return (
-    <>
-      <h1>NEWS Component</h1>
-
-      <h3>Novidades!</h3>
-      <h1>Não perca essas novidades!!</h1>
-      <h2>por Marcus Vinícius</h2>
-      <h3>Atualizado em 21/Maio/2022</h3>
-      <h3>AQUI VAI UMA IMAGEM</h3>
-    </>
+    <article id='newsContainer'>
+      <header>
+        <hgroup>
+          <h3>Novidades!</h3>
+          <h1>Não perca essas novidades!!</h1>
+          <h2>por Marcus Vinícius</h2>
+          <h3 className='alignRight'>Atualizado em 26/Julho/2022</h3>
+          <h3>AQUI VAI UMA IMAGEM</h3>
+        </hgroup>
+      </header>
+    </article>
   );
 };
 
