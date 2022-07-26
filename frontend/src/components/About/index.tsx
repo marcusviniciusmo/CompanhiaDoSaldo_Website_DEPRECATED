8ab7227d731f12dd648afc37da9ea89c72cae023
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-07-26 07:50:18 +0100

    About component styling

--- frontend/src/components/About/index.tsx
+++ frontend/src/components/About/index.tsx
@@ -1,17 +1,21 @@
 import { AboutData } from 'utils/Mocks/About';
+import './styles.css';
 
 function About() {
   return (
-    <>
-      <h1>ABOUT Component</h1>
-      <h2>{AboutData.Title}</h2>
+    <article id='aboutContainer'>
+      <h2 className='titleArticle'>{AboutData.Title}</h2>
       <p>{AboutData.Text}</p>
 
-      <img src={AboutData.Image.Url} alt={AboutData.Image.Description} />
+      <figure id="photoAbout">
+        <img src={AboutData.Image.Url} alt={AboutData.Image.Description} />
 
-      <h3>{AboutData.FigcaptionTitle}</h3>
-      <p>{AboutData.FigcaptionText}</p>
-    </>
+        <figcaption>
+          <h3>{AboutData.FigcaptionTitle}</h3>
+          <p>{AboutData.FigcaptionText}</p>
+        </figcaption>
+      </figure>
+    </article>
   );
 };
 
