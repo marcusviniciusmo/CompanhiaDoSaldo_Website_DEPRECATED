60ce422715c548920f2e40375db3f733e5e7bc9a
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-07-30 19:05:53 +0100

    Identification component creation

---
+++ frontend/src/components/Identification/index.tsx
@@ -0,0 +1,22 @@
+function Identification() {
+  return (
+    <>
+      IDENTIFICATION Component
+
+      <h3>Identificação do Cliente</h3>
+      <h4>Nome:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>E-mail:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>Sexo:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>Masculino</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+      <h4>Feminino</h4>
+      <h4>Data de Nascimento:</h4>
+      <h4>AQUI VAI UM CAMPO</h4>
+    </>
+  );
+};
+
+export default Identification;
