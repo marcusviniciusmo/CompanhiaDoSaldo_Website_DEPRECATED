6c690f4e1720b49b288f2a23157df90702d84ccb
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-07-31 18:34:18 +0100

    Creating the routes for the Iframe views in the Pedidos view

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -4,6 +4,10 @@ import Produtos from 'views/Produtos';
 import Fornecedores from 'views/Fornecedores';
 import Pedidos from 'views/Pedidos';
 import Contato from 'views/Contato';
+import Address from 'components/Address';
+import Identification from 'components/Identification';
+import Message from 'components/Message';
+import Product from 'components/Product';
 
 function Router() {
   return (
@@ -14,6 +18,10 @@ function Router() {
         <Route path='/fornecedores' element={<Fornecedores />} />
         <Route path='/pedidos' element={<Pedidos />} />
         <Route path='/contato' element={<Contato />} />
+        <Route path='/pedidos/address' element={<Address />}/>
+        <Route path='/pedidos/identification' element={<Identification />} />
+        <Route path='/pedidos/message' element={<Message />}/>
+        <Route path='/pedidos/Product' element={<Product />} />
       </Routes>
     </BrowserRouter>
   );
